202b8dabf6c3bd5e8931318469b6771f505798de
Andrew Van Valkenburgh <andrewvv@byu.edu>
2024-02-24 14:25:40 -0700

    Project 3: Login service tests created and passing

---
+++ server/src/test/java/serviceTests/LoginServiceTests.java
@@ -0,0 +1,49 @@
+package serviceTests;
+
+import dataAccess.*;
+import model.request.LoginRequest;
+import model.result.LoginResult;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import service.UserService;
+
+public class LoginServiceTests {
+
+    @Test
+    void loginServiceSuccess() throws DataAccessException {
+        // create new databases and initialize UserService
+        UserDAO userDAO = new UserDAOMemory();
+        AuthDAO authDAO = new AuthDAOMemory();
+        UserService userService = new UserService(userDAO, authDAO);
+
+        // create the user information
+        userDAO.createUser("TestUsername", "TestPassword", "Test@Email");
+
+        // make sure the function does not throw an exception and returns the correct response object.
+        LoginRequest reg = new LoginRequest("TestUsername", "TestPassword");
+        var res = userService.login(reg);
+        Assertions.assertEquals("TestUsername", res.username());
+        Assertions.assertEquals(LoginResult.class, res.getClass());
+        Assertions.assertNotEquals("", res.authToken());
+        Assertions.assertNotEquals(null, res.authToken());
+    }
+
+    @Test
+    void loginServiceErrors() throws DataAccessException {
+        // create new databases and initialize UserService
+        UserDAO userDAO = new UserDAOMemory();
+        AuthDAO authDAO = new AuthDAOMemory();
+        UserService userService = new UserService(userDAO, authDAO);
+
+        // create the user information
+        userDAO.createUser("TestUsername", "TestPassword", "Test@Email");
+
+        // should throw an error when password is incorrect
+        LoginRequest reg = new LoginRequest("TestUsername", "TestWrongPassword");
+        Assertions.assertThrows(DataAccessException.class, () -> userService.login(reg));
+
+        // should throw an error when user does not exist
+        LoginRequest newReg = new LoginRequest("TestWrongUsername", "TestPassword");
+        Assertions.assertThrows(DataAccessException.class, () -> userService.login(newReg));
+    }
+}
