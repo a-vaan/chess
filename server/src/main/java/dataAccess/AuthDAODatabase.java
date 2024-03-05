55c2d39ce23a7e17ffa9cba0eb71d102fca025d7
Andrew Van Valkenburgh <andrewvv@byu.edu>
2024-03-04 22:59:35 -0700

    Project 4: Started working on AuthDAODatabase class

---
+++ server/src/main/java/dataAccess/AuthDAODatabase.java
@@ -0,0 +1,60 @@
+package dataAccess;
+
+import dataAccess.DAOInterfaces.AuthDAO;
+import model.AuthData;
+
+import java.sql.SQLException;
+
+public class AuthDAODatabase implements AuthDAO {
+
+    public AuthDAODatabase() throws DataAccessException {
+        configureDatabase();
+    }
+
+    @Override
+    public String createAuth(String username) {
+        return null;
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) {
+        return null;
+    }
+
+    @Override
+    public void deleteAuth(String authToken) {
+
+    }
+
+    @Override
+    public void deleteAllAuths() {
+
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  pet (
+              `id` int NOT NULL AUTO_INCREMENT,
+              `name` varchar(256) NOT NULL,
+              `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`id`),
+              INDEX(type),
+              INDEX(name)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
