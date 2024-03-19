bdf34a8c7c359e110f6dbf3433ee5f9bec594839
Andrew Van Valkenburgh <andrewvv@byu.edu>
2024-03-19 16:20:25 -0600

    Project 5: Register and login success test cases created and passed

---
+++ client/src/main/java/server/ServerFacade.java
@@ -0,0 +1,133 @@
+package server;
+
+import com.google.gson.Gson;
+import model.result.LoginResult;
+import model.result.RegisterResult;
+
+import java.io.*;
+import java.net.*;
+import java.util.Map;
+
+public class ServerFacade {
+
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+    public RegisterResult register(String username, String password, String email) throws ResponseException {
+        try {
+            URL url = (new URI(serverUrl + "/user")).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod("POST");
+            http.setDoOutput(true);
+
+            http.addRequestProperty("Content-Type", "application/json");
+            var body = Map.of("username", username,
+                    "password", password,
+                    "email", email);
+            try (var outputStream = http.getOutputStream()) {
+                var jsonBody = new Gson().toJson(body);
+                outputStream.write(jsonBody.getBytes());
+            }
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, RegisterResult.class);
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    public LoginResult login(String username, String password) throws ResponseException {
+        try {
+            URL url = (new URI(serverUrl + "/session")).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod("POST");
+            http.setDoOutput(true);
+
+            http.addRequestProperty("Content-Type", "application/json");
+            var body = Map.of("username", username,
+                    "password", password);
+            try (var outputStream = http.getOutputStream()) {
+                var jsonBody = new Gson().toJson(body);
+                outputStream.write(jsonBody.getBytes());
+            }
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, LoginResult.class);
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    public void deletePet(int id) throws ResponseException {
+        var path = String.format("/pet/%s", id);
+        this.makeRequest("DELETE", path, null, null);
+    }
+
+    public void deleteAllPets() throws ResponseException {
+        var path = "/pet";
+        this.makeRequest("DELETE", path, null, null);
+    }
+
+//    public Pet[] listPets() throws server.ResponseException {
+//        var path = "/pet";
+//        record listPetResponse(Pet[] pet) {
+//        }
+//        var response = this.makeRequest("GET", path, null, listPetResponse.class);
+//        return response.pet();
+//    }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws ResponseException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            throw new ResponseException(status, "failure: " + status);
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+}
--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -1,18 +1,20 @@
-package java.clientTests;
+package clientTests;
 
 import org.junit.jupiter.api.*;
 import server.Server;
-
+import server.ServerFacade;
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("http://localhost:" + port);
     }
 
     @AfterAll
@@ -22,8 +24,16 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    void registerSuccess() throws Exception {
+        var authData = facade.register("playerRegister", "passwordRegister", "pR@email.com");
+        Assertions.assertTrue(authData.authToken().length() > 10);
+    }
+
+    @Test
+    void loginSuccess() throws Exception {
+        facade.register("playerLogin", "passwordLogin", "pL@email.com");
+        var loginData = facade.login("playerLogin", "passwordLogin");
+        Assertions.assertTrue(loginData.authToken().length() > 10);
     }
 
 }
